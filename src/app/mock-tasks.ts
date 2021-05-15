567d48016a8004912ef1f40983e67d529958b355
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-05-15 10:44:55 -0700

    adding tasks and playing with font icons

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import { Task } from './Tasks';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Doctor's Appointment",
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Go to Fresno',
+    day: 'May 15th at 10:00am',
+    reminder: true,
+  },
+];
